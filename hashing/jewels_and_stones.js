6d56d1de39b2f044b8507a02f921b2657ca6d28d
Victor Gonzalez <victorfdo.gm@gmail.com>
2025-11-10 13:32:44 -0600

    Problem for hashing/jewels_and_stones.js

---
+++ hashing/jewels_and_stones.js
@@ -0,0 +1,21 @@
+// You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    
+    let jewelsSet = new Set(jewels);
+
+    let res = 0;
+    for (const element of stones) {
+        if (jewelsSet.has(element)) {
+            res++;
+        }
+    }
+    return res;
+};
