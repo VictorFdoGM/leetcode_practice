f77c8b86432051322f8813e47c93b0ad4a36b5c3
Victor Gonzalez <victorfdo.gm@gmail.com>
2025-11-13 12:40:17 -0600

    Problem for trees_graphs/validate-binary-search-tree.js add Leetcode example iterative approach

--- trees_graphs/validate-binary-search-tree.js
+++ trees_graphs/validate-binary-search-tree.js
@@ -40,6 +40,30 @@ var isValidBST = function(root, min = -Infinity, max = Infinity) {
 };
 
 /**
+ * Leetcode example iterative approach
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    let stack = [[root, -Infinity, Infinity]];
+    while (stack.length) {
+        let [node, small, large] = stack.pop();
+        if (small >= node.val || node.val >= large) {
+            return false;
+        }
+        
+        if (node.left) {
+            stack.push([node.left, small, node.val]);
+        }
+        if (node.right) {
+            stack.push([node.right, node.val, large]);
+        }
+    }
+    
+    return true;
+};
+
+/**
  * Leetcode submission
  * function TreeNode(val, left, right) {
  *     this.val = (val===undefined ? 0 : val)
