6726344691a17bd4a607a84c91054ea3a5e1613e
Victor Gonzalez <victorfdo.gm@gmail.com>
2025-11-22 21:12:51 -0600

    all-nodes-distance-k-in-binary-tree.js leet code solution

--- trees_graphs/all-nodes-distance-k-in-binary-tree.js
+++ trees_graphs/all-nodes-distance-k-in-binary-tree.js
@@ -11,6 +11,7 @@
  * }
  */
 /**
+ * My solution
  * @param {TreeNode} root
  * @param {TreeNode} target
  * @param {number} k
@@ -76,4 +77,50 @@ var distanceK = function(root, target, k) {
 
     seen[target.val] = true;
     return graphBfs(target.val);
+};
+
+
+
+/**
+ * Leet code solution
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+var distanceK = function(root, target, k) {
+    let dfs = (node, parent) => {
+        if (!node) {
+            return;
+        }
+        
+        node.parent = parent;
+        dfs(node.left, node);
+        dfs(node.right, node);
+    }
+    
+    dfs(root, null);
+    let queue = [target];
+    let seen = new Set([target]);
+    let distance = 0;
+    
+    while (queue.length && distance < k) {
+        let currentLength = queue.length;
+        let nextQueue = [];
+        
+        for (let i = 0; i < currentLength; i++) {
+            let node = queue[i];
+            for (const neighbor of [node.left, node.right, node.parent]) {
+                if (neighbor && !seen.has(neighbor)) {
+                    seen.add(neighbor);
+                    nextQueue.push(neighbor);
+                }
+            }
+        }
+        
+        queue = nextQueue;
+        distance++;
+    }
+    
+    return queue.map(node => node.val);
 };
