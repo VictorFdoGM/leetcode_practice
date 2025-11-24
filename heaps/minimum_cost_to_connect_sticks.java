7ca48c52f3dcc334cb9884047e0ab3bc86b26bfb
Victor Gonzalez <victorfdo.gm@gmail.com>
2025-11-23 20:50:59 -0600

    heaps/minimum_cost_to_connect_sticks.java

---
+++ heaps/minimum_cost_to_connect_sticks.java
@@ -0,0 +1,28 @@
+// Minimum Cost to Connect Sticks
+// You have some number of sticks with positive integer lengths. These lengths are given as an array sticks, where sticks[i] is the length of the ith stick.
+
+// You can connect any two sticks of lengths x and y into one stick by paying a cost of x + y. You must connect all the sticks until there is only one stick remaining.
+
+// Return the minimum cost of connecting all the given sticks into one stick in this way.
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public int connectSticks(int[] sticks) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for (int stick : sticks) {
+            heap.add(stick);    
+        }
+
+        int minCost = 0;
+        while (heap.size() > 1) {
+            int a = heap.remove();
+            int b = heap.remove();
+            minCost += a + b;
+            heap.add(a + b);
+        }
+
+        return minCost;
+    }
+}
