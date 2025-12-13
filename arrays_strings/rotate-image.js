9ae83c6ea6759750390d180a54c65b356c0a5a05
Victor Gonzalez <victorfdo.gm@gmail.com>
2025-12-12 19:33:10 -0600

    arrays_strings/rotate-image.js

---
+++ arrays_strings/rotate-image.js
@@ -0,0 +1,44 @@
+// Rotate Image
+// You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    let n = matrix.length;
+    for (let i = 0; i < Math.floor(n / 2); i++) {
+        for (let j = 0; j < Math.floor((n + 1) / 2); j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[n - 1 - j][i];
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = temp;
+        }
+    }
+};
+
+var rotate = function (matrix) {
+    const n = matrix.length;
+    // Transpose
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+    // Reflect
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n / 2; j++) {
+            [matrix[i][j], matrix[i][n - 1 - j]] = [
+                matrix[i][n - 1 - j],
+                matrix[i][j],
+            ];
+        }
+    }
+};
+
+console.log(rotate([[1,2,3],[4,5,6],[7,8,9]]));
+// console.log(rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]));
